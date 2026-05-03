7390467503d7f3b8a0939bd31471d3c733a0464e
ntamratsvg <ntamrat@students.kennesaw.edu>
2026-05-03 13:27:37 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Function to handle the quiz submission
+function checkQuiz() {
+    alert("Thank you for taking the quiz! Results are being processed.");
+    // You can add logic here to check for correct radio button values
+}
+
+console.log("Responsive IT 3203 Site Loaded.");
